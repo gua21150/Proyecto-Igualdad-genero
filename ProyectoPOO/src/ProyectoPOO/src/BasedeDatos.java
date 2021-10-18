4092e90b2d9db9a5d05a9780344895d4eaa7a721
Arobelcam <aaronbeltrancampos@gmail.com>
2021-10-17 20:10:07 -0600

    Importacion de librerias sql
    Se ha colocado una carpeta con el java connector el cual sera fundamental para conectar la clase de BasedeDatos con la ruta de la base de datos en My SQl

--- ProyectoPOO/src/ProyectoPOO/src/BasedeDatos.java
+++ ProyectoPOO/src/ProyectoPOO/src/BasedeDatos.java
@@ -1,3 +1,17 @@
+package ProyectoPOO.src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class BasedeDatos {
     
+    conexion con;
+
+    public BasedeDatos(){
+        try {
+            Class.forName("java driver");
+            con = DriverManager("datos de la conexion");
+        } catch (Exception e) {
+            
+        }
+    }
 }
