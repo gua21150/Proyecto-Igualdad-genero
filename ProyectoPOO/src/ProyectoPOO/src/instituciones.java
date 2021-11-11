b1cf05dad5385a6deb7f3634f3eb42c5e84db9d1
Raul <san21401@uvg.edu.gt>
2021-11-10 19:29:46 -0600

    estructura pagina instituciones
    Se realizó la estructura de la pagina instituciones y se hizo la clase controladora de esta pagina fxml

---
+++ ProyectoPOO/src/ProyectoPOO/src/instituciones.java
@@ -0,0 +1,65 @@
+package ProyectoPOO.src;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class instituciones {
+    
+    @FXML
+    private Button actividades;
+
+    @FXML
+    private Button instituciones;
+
+    @FXML
+    private Button lecturas;
+
+
+    @FXML
+    private Button principal;
+
+    @FXML
+    void Onactividades(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Oninstituciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Onlecturas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Onprincipal(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void onlink1(ActionEvent event) throws URISyntaxException, IOException {
+    
+
+    }
+
+    @FXML
+    void onlink2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onlink3(ActionEvent event) {
+
+    }
+
+    
+}
