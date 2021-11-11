9a35acdbc228ea3cbace6d3eae82fad125ede80b
Raul <san21401@uvg.edu.gt>
2021-11-11 08:25:24 -0600

    conectividad interfaces
    Se configuro para que hubiera comunicación entre la pagina inicial y la interfaz de instituciones. Además se configuro para que al momento de tocar lo hipervinculos mande a las diferentes páginas web

--- ProyectoPOO/src/ProyectoPOO/src/instituciones.java
+++ ProyectoPOO/src/ProyectoPOO/src/instituciones.java
@@ -10,13 +10,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import java.awt.Desktop;
+import java.net.URI;
 public class instituciones {
     
     @FXML
     private Button actividades;
-
-    @FXML
-    private Button instituciones;
 
     @FXML
     private Button lecturas;
@@ -28,17 +27,34 @@ public class instituciones {
     @FXML
     void Onactividades(ActionEvent event) throws IOException{
         
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("paginainicial.fxml"));
+            Stage stage0 = (Stage) actividades.getScene().getWindow();
+            Stage stage1 = new Stage();
+            stage0.close();
+            stage1.setScene(new Scene(root));
+            stage1.show();
+        }catch(Exception e){
+            System.out.println("algo fallo: "+e.getMessage());
+        }
 
-    }
-
-    @FXML
-    void Oninstituciones(ActionEvent event) throws IOException{
-
-        
     }
 
     @FXML
     void Onlecturas(ActionEvent event) throws IOException{
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("paginainicial.fxml"));
+            Stage stage0 = (Stage) lecturas.getScene().getWindow();
+            Stage stage1 = new Stage();
+            stage0.close();
+            stage1.setScene(new Scene(root));
+            stage1.show();
+        }catch(Exception e){
+            System.out.println("algo fallo: "+e.getMessage());
+        }
+
 
     }
 
@@ -47,7 +63,7 @@ public class instituciones {
 
         try
         {
-            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("paginainicial.fxml"));
             Stage stage0 = (Stage) principal.getScene().getWindow();
             Stage stage1 = new Stage();
             stage0.close();
@@ -60,17 +76,19 @@ public class instituciones {
     
     @FXML
     void onlink1(ActionEvent event) throws URISyntaxException, IOException {
-    
+    Desktop.getDesktop().browse(new URI("http://ggm.org.gt/caimus/"));
 
     }
 
     @FXML
-    void onlink2(ActionEvent event) {
+    void onlink2(ActionEvent event) throws URISyntaxException, IOException {
+    Desktop.getDesktop().browse(new URI("https://miriam-guatemala.org/"));
 
     }
 
     @FXML
-    void onlink3(ActionEvent event) {
+    void onlink3(ActionEvent event) throws URISyntaxException, IOException{
+        Desktop.getDesktop().browse(new URI("https://institutodelavictima.gob.gt/quienes-somos/"));
 
     }
 
--- ProyectoPOO/src/ProyectoPOO/src/paginainicial.java
+++ ProyectoPOO/src/ProyectoPOO/src/paginainicial.java
@@ -51,7 +51,17 @@ public class paginainicial {
 
     @FXML
     void oninstituciones(ActionEvent event) {
-       
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("instituciones.fxml"));
+            Stage stage0 = (Stage) instituciones.getScene().getWindow();
+            Stage stage1 = new Stage();
+            stage0.close();
+            stage1.setScene(new Scene(root));
+            stage1.show();
+        }catch(Exception e){
+            System.out.println("algo fallo: "+e.getMessage());
+        }
 
     }
 
