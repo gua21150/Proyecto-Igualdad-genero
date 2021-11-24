efdfa75a0bfea8a4df8307036e8bb9ba3eb7b492
Mariel Guamuche <80536944+gua21150@users.noreply.github.com>
2021-11-24 12:50:46 -0600

    Update Lecturas1.java

--- ProyectoPOO/src/ProyectoPOO/src/Lecturas1.java
+++ ProyectoPOO/src/ProyectoPOO/src/Lecturas1.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.awt.Desktop;
 import java.net.URISyntaxException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -63,6 +62,4 @@ public class Lecturas1 {
     void Onclick1(ActionEvent event) throws URISyntaxException, IOException {
         Desktop.getDesktop().browse(new URI("https://scielo.isciii.es/scielo.php?script=sci_arttext&pid=S1132-05592006000200004"));
     }
-
-
 }
